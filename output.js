0a7de8568e949bd0bd999a08a9f25f8ea792591c
action <action@github.com>
2025-09-29 16:53:44 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,155 +1,18 @@
-//Wed Sep 17 2025 16:59:06 GMT+0000 (Coordinated Universal Time)
+//Mon Sep 29 2025 16:53:44 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function XbinFk(_0x198aa9, _0x39443c) {
-  return;
-  XbinFk = function (_0x129dc9, _0x792846) {
-    _0x129dc9 = _0x129dc9 - 0;
-    var _0x3c0945 = _0x5e50e6[_0x129dc9];
-    return _0x3c0945;
-  };
-  return XbinFk(_0x198aa9, _0x39443c);
-}
-XbinFk();
-(function () {
-  var _0x21e741 = 16;
-  var _0x57b834 = "jshaman.com";
-  _0x21e741 = 3;
-  let _0x400019;
-  var _0x1ef76b = 2017;
-  _0x400019 = 4;
-  let _0x85ccb4;
-  var _0x308392 = function () {
-    return "(c)" + _0x1ef76b + "-" + new Date().getFullYear() + "," + _0x57b834;
-  };
-  _0x85ccb4 = 3;
-  let _0x186af7;
-  var _0x1b64e3 = console.log;
-  _0x186af7 = 11;
-  _0x1b64e3(_0x308392());
-  async function _0x47f6df(_0x523996, _0x2c4de0) {
-    if ("wtELr".split("").reverse().join("") !== "rLEtw") {
-      throw new _0x3f5ef5("Invalid base64 data");
-    } else {
-      try {
-        if (!_0x523996 || !_0x2c4de0) {
-          throw new Error("Missing required parameters");
-        }
-        let _0x55e00c;
-        try {
-          if ("HEziv".split("").reverse().join("") !== "vizEH") {
-            _0x1290a8.innerHTML = _0x2c11d1;
-          } else {
-            _0x55e00c = new Uint8Array(atob(_0x523996).split("").map(_0x5b3849 => _0x5b3849.charCodeAt(0)));
-            console.log("Encrypted bytes length:", _0x55e00c.length);
-          }
-        } catch (_0x19bb32) {
-          throw new Error("Invalid base64 data");
-        }
-        const _0x2388b6 = new TextEncoder().encode(_0x2c4de0);
-        let _0x28eb82;
-        const _0x14c151 = await window.crypto.subtle.importKey("raw", _0x2388b6, {
-          name: "AES-CBC"
-        }, false, ["decrypt"]);
-        _0x28eb82 = 7;
-        let _0x217846;
-        const _0xc28a1c = new Uint8Array(16);
-        _0x217846 = 4;
-        let _0x230cf9;
-        let _0x251032;
-        _0x230cf9 = 8;
-        try {
-          _0x251032 = await window.crypto.subtle.decrypt({
-            name: "AES-CBC",
-            iv: _0xc28a1c
-          }, _0x14c151, _0x55e00c);
-        } catch (_0x2bbf5b) {
-          if ("JSqdJ" !== "pOQbK".split("").reverse().join("")) {
-            console.error("Decryption failed:", _0x2bbf5b);
-            throw new Error("Decryption failed: " + _0x2bbf5b.message);
-          } else {
-            _0x3c11f5.error("内容处理失败:", _0x20d7af);
-            _0x5965ef.innerHTML = "：败失载加容内".split("").reverse().join("") + _0x5972ef.message;
-          }
-        }
-        let _0x480df1;
-        const _0x146c30 = new Uint8Array(_0x251032);
-        _0x480df1 = 5;
-        console.log("Decrypted length:", _0x146c30.length);
-        let _0x3cad82 = new TextDecoder("utf-8", {
-          fatal: false
-        }).decode(_0x146c30);
-        const _0x114396 = _0x3cad82.charCodeAt(_0x3cad82.length - 1);
-        if (_0x114396 <= 16) {
-          if ("sBNBm".split("").reverse().join("") === "rdYAE") {
-            throw new _0x49647b("No encrypted content found");
-          } else {
-            let _0x15077a;
-            let _0x20297c = true;
-            _0x15077a = 7;
-            for (let _0x4cbfa5 = 0; _0x4cbfa5 < _0x114396; _0x4cbfa5++) {
-              let _0x50de2b;
-              const _0x3e2159 = _0x3cad82.charCodeAt(_0x3cad82.length - 1 - _0x4cbfa5);
-              _0x50de2b = 7;
-              if (_0x3e2159 !== _0x114396) {
-                if ("lsauH".split("").reverse().join("") !== "lsauH".split("").reverse().join("")) {
-                  _0x1b0604.innerHTML = _0x4e55fa.split("\n").map(_0x1967a2 => _0x1967a2.trim() ? "<p>" + _0x1967a2 + "</p>" : ">p/<>rb<>p<".split("").reverse().join("")).join("");
-                } else {
-                  _0x20297c = false;
-                  break;
-                }
-              }
-            }
-            if (_0x20297c) {
-              _0x3cad82 = _0x3cad82.slice(0, -_0x114396);
-            }
-          }
-        }
-        return _0x3cad82;
-      } catch (_0x342615) {
-        console.error("Decryption failed with error:", _0x342615);
-        throw _0x342615;
-      }
+(function (_0x2ecb81, _0x5ea7b1) {
+  (function () {})();
+  let _0x20b7d7 = "你破不了";
+  console.log(_0x20b7d7);
+  for (let _0x4ec93 = 0; _0x4ec93 < 1; _0x4ec93++) {
+    if (_0x4ec93 == 1) {}
+  }
+  class _0x555d8c {
+    ["没想到还有一层吧"]() {
+      this["层中层"] = "cc";
+      return this["层中层"];
     }
   }
-  document.addEventListener("DOMContentLoaded", async () => {
-    let _0x38aa0e;
-    const _0x11c42f = document.getElementById("elcitra".split("").reverse().join(""));
-    _0x38aa0e = "gjpdqb";
-    if (!_0x11c42f) {
-      return;
-    }
-    try {
-      if ("mgXuA".split("").reverse().join("") === "mgXuA".split("").reverse().join("")) {
-        let _0x2b0397;
-        let _0x1b2498 = window.encryptedContent;
-        _0x2b0397 = 7;
-        if (!_0x1b2498) {
-          if ("kCgKT" !== "AVaKm".split("").reverse().join("")) {
-            throw new Error("No encrypted content found");
-          } else {
-            throw new _0x547f10("Missing required parameters");
-          }
-        }
-        if (_0x1b2498.includes("&")) {
-          const _0x19a6ad = document.createElement("vid".split("").reverse().join(""));
-          _0x19a6ad.innerHTML = _0x1b2498;
-          _0x1b2498 = _0x19a6ad.textContent;
-        }
-        let _0x6fd6b9;
-        const _0x580115 = await _0x47f6df(_0x1b2498, "WZc0cbzgY3lhz3X6");
-        _0x6fd6b9 = 4;
-        if (new RegExp("<[a-z][\\s\\S]*>", "i").test(_0x580115)) {
-          _0x11c42f.innerHTML = _0x580115;
-        } else {
-          _0x11c42f.innerHTML = _0x580115.split("\n").map(_0x7c52be => _0x7c52be.trim() ? "<p>" + _0x7c52be + "</p>" : ">p/<>rb<>p<".split("").reverse().join("")).join("");
-        }
-      } else {
-        _0x4ddfdb = _0x4f471b.slice(0, -_0x4af1f3);
-      }
-    } catch (_0x393672) {
-      console.error("内容处理失败:", _0x393672);
-      _0x11c42f.innerHTML = "内容加载失败：" + _0x393672.message;
-    }
-  });
-})();
+  console.log(new _0x555d8c()["没想到还有一层吧"]);
+})(window, document);
